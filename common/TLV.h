40ef0792c9e13b921e48809e66c0f0869deea336
Cristian Traina <crissstian96@gmail.com>
2020-09-25 17:48:05 +0200

    common

---
+++ common/TLV.h
@@ -0,0 +1,13 @@
+#include <boost/any.hpp>
+#include <map>
+
+class TLV {
+    char* encode(boost::any plain) {
+        std::string str;
+        str.append()
+    }
+
+    std::map<std::string, boost::any> decode(char* encoded) {
+
+    }
+};
